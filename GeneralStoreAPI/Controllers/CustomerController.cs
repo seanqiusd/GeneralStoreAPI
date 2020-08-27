616f7ed9810aece6162e551185748c6cedc892ec
seansql <shawnq2116@gmal.com>
2020-08-27 12:02:13 -0400

    Defined Contorllers, need to build CRUD

---
+++ GeneralStoreAPI/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using GeneralStoreAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GeneralStoreAPI.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        private StoreDbContext _context = new StoreDbContext();
+
+        // Post
+        
+        // Get
+
+        // Get{id}
+
+        // Put {id}
+
+        // Delete {id}
+
+
+    }
+}
---
+++ GeneralStoreAPI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using GeneralStoreAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GeneralStoreAPI.Controllers
+{
+    public class ProductController : ApiController
+    {
+        private StoreDbContext _context = new StoreDbContext();
+
+        // Post
+
+        // Get
+
+        // Get{id}
+
+        // Put {id}
+
+        // Delete {id}
+    }
+}
