36157efd33f3cd26313a35f9370533c3955e216e
seansql <shawnq2116@gmal.com>
2020-08-27 15:46:00 -0400

    Added TransactionController and built out and tested

--- GeneralStoreAPI/Controllers/TransactionController.cs
+++ GeneralStoreAPI/Controllers/TransactionController.cs
@@ -13,10 +13,42 @@ namespace GeneralStoreAPI.Controllers
         private StoreDbContext _context = new StoreDbContext();
 
         // Post
+        public IHttpActionResult Post(Transaction transaction)
+        {
+            if (transaction == null)
+            {
+                return BadRequest("Your request body cannot be empty");
+            }
+            if (ModelState.IsValid && transaction.CustomerId != 0 && transaction.ProductSKU != null)
+            {
+                _context.Transactions.Add(transaction);
+                _context.SaveChanges();
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
 
         // Get
+        public IHttpActionResult Get()
+        {
+            List<Transaction> transactions = _context.Transactions.ToList();
+            if (transactions.Count != 0)
+            {
+                return Ok(transactions);
+            }
+            return BadRequest("Your database contains no Transactins...DO SOME WORK");
+        }
 
         // Get{id}
+        public IHttpActionResult Get(string sku)
+        {
+            Transaction transactions = _context.Transactions.Find(sku);
+            if (transactions == null)
+            {
+                return NotFound();
+            }
+            return Ok(transactions);
+        }
 
         // Put {id}
 
