695b39607182493b3372cc9d4691ab7a0a3355ee
seansql <shawnq2116@gmal.com>
2020-08-27 15:15:31 -0400

    Added More to CustomerController and ProductController

--- GeneralStoreAPI/Controllers/CustomerController.cs
+++ GeneralStoreAPI/Controllers/CustomerController.cs
@@ -12,15 +12,54 @@ namespace GeneralStoreAPI.Controllers
     {
         private StoreDbContext _context = new StoreDbContext();
 
-        // Post
+        // Post --Create
+        public IHttpActionResult Post(Customer customer)
+        {
+
+            // if an empty Customer is passed in (returned) // validations we are passing in; we do validations 
+            if (customer == null)
+            {
+                return BadRequest("Your request body cannot be empty.");
+            }
+
+            // if the ModelState is not Valid // validations we are passing in 
+            if (ModelState.IsValid) // .IsValid is just another way of saying == true; ModelState is inherited from ApiController
+            {
+                _context.Customers.Add(customer); // Adding customer to dbset // and to make this persist, we have to save next
+                _context.SaveChanges(); // save changes
+                return Ok(); // return an ok
+            }
+            return BadRequest(ModelState); // if none of the previous two ifs hit, then return BadRequest
+        }
         
-        // Get
+        // Get --Read All
+        public IHttpActionResult Get()
+        {
+            List<Customer> customers = _context.Customers.ToList();
+            if (customers.Count != 0)
+            {
+                return Ok(customers);
 
-        // Get{id}
+            }
+            return BadRequest("Your database contains no Customers");
+        }
 
-        // Put {id}
+        // Get{id} --Read By ID
+        public IHttpActionResult Get(int id)
+        {
+            
+            Customer customer = _context.Customers.Find(id);
+            // if id is 0
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
 
-        // Delete {id}
+        // Put {id} --Update
+
+        // Delete {id} --Delete By ID
 
 
     }
--- GeneralStoreAPI/Controllers/ProductController.cs
+++ GeneralStoreAPI/Controllers/ProductController.cs
@@ -12,14 +12,67 @@ namespace GeneralStoreAPI.Controllers
     {
         private StoreDbContext _context = new StoreDbContext();
 
+
+
         // Post
+        public IHttpActionResult Post(Product product)
+        {
+            if (product == null)
+            {
+                return BadRequest("Your request body cannot be empty");
+            }
+            product.SKU = GenerateSku(product.Name); // needed to assign the sku value to the product
+            if (ModelState.IsValid && product.SKU != null)
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
 
         // Get
+        public IHttpActionResult Get()
+        {
+            List<Product> products = _context.Products.ToList();
+            if (products.Count != 0)
+            {
+                return Ok(products);
+            }
+            return BadRequest("Your database contains no Customers");
+        }
 
         // Get{id}
+        public IHttpActionResult Get(string sku)
+        {
+            Product product = _context.Products.Find(sku);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
 
         // Put {id}
 
         // Delete {id}
+
+
+        private string GenerateSku(string productName) // This was given to us by Joshua; we copied and pasted
+        {
+            // Initialize a Random object so we can randomly assign this a number
+            Random random = new Random();
+            // Get a Random number and turn it into a string
+            var randItemNum = random.Next(0, 1000).ToString();
+            // Construct a 3 character string based on the above number. If the number is less than 3 digits, add 0's in front of it to make it 3 digits
+            var itemId = new string('0', 3 - randItemNum.Length) + randItemNum;
+            // Create the entire SKU and return it
+            return $"EFA-{productName.Substring(0, 3)}-{itemId}";
+        }
+
+
+
+
+
     }
 }
